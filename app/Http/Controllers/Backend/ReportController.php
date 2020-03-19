c5e8cf3443f890a50dc934019411102f09d4ea20
Oat <hlkittipan@gmail.com>
2020-03-19 17:38:18 +0700

    logo

--- app/Http/Controllers/Backend/ReportController.php
+++ app/Http/Controllers/Backend/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
@@ -12,12 +13,18 @@ class ReportController extends Controller
     {
         if (isset($request->dates)) {
             //dd($request->has('dates'));
-            "SELECT  " .
-            "Year(quo_date) as year, MONTH(quo_date) as month, COUNT(qd.product_id)  " .
-            "FROM " .
-            "quotations AS q " .
-            "INNER JOIN quotation_details AS qd ON q.id = qd.quo_id " .
-            "GROUP BY MONTH(quo_date) ,Year(quo_date)";
+            $date = explode(" - ",$request->dates);
+            $start = changeFormatDate($date[0],'Y-m-d');
+            $end = changeFormatDate($date[1],'Y-m-d');
+
+            $data = DB::table('quotations as q')
+                ->join('quotation_details as qd','q.id','=','qd.quo_id')
+                ->whereBetween('q.quo_date',[$start,$end])
+                ->groupBy(DB::raw('MONTH(q.quo_date)' ,'Year(q.quo_date)'))
+                ->selectRaw('Year(q.quo_date) as year, MONTH(q.quo_date) as month, COUNT(qd.product_id)'  )
+                ->orderBy('year','asc')
+                ->get();
+            //dd($data);
         }
 
         return view('backends.report.sales');
